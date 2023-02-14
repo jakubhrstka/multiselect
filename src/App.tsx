809ce46b4a3009c9c766bbce2d50f19b3dda11ee
Jakub Hrstka <jakubhrstkaa@gmail.com>
2023-02-14 22:05:18 +0100

    headline color

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Multiselect from "./components/Multiselect/Multiselect";
 function App() {
   return (
     <main className="bg-slate-800 p-6 h-screen w-screen flex flex-col justify-center items-center">
-      <h1>Multiselect</h1>
+      <h1 className="text-white">Multiselect</h1>
       <Multiselect />
     </main>
   );
